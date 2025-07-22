195b3c54e88fe60d4651849acc025794f7a2677d
Eveeee <44003480+katsu-chan@users.noreply.github.com>
2025-07-23 02:36:59 +0300

    add basics

--- nix-on-droid.nix
+++ nix-on-droid.nix
@@ -5,25 +5,31 @@
   environment.packages = with pkgs; [
     # User-facing stuff that you really really want to have
     vim # or some other editor, e.g. nano or neovim
+    neovim
+    nano
+
+    zsh
+
+    git
 
     # Some common stuff that people expect to have
-    #procps
-    #killall
-    #diffutils
-    #findutils
-    #utillinux
-    #tzdata
-    #hostname
-    #man
-    #gnugrep
-    #gnupg
-    #gnused
-    #gnutar
-    #bzip2
-    #gzip
-    #xz
-    #zip
-    #unzip
+    procps
+    killall
+    diffutils
+    findutils
+    utillinux
+    tzdata
+    hostname
+    man
+    gnugrep
+    gnupg
+    gnused
+    gnutar
+    bzip2
+    gzip
+    xz
+    zip
+    unzip
   ];
 
   # Backup etc files instead of failing to activate generation if a file already exists in /etc
@@ -38,5 +44,5 @@
   '';
 
   # Set your time zone
-  #time.timeZone = "Europe/Berlin";
+  time.timeZone = "Etc/GMT-3";
 }
