40a72ecaa0be72c186019dd88e6403e23319401b
Eveeee <44003480+katsu-chan@users.noreply.github.com>
2025-08-21 01:23:22 +0300

    did something?

--- flake.nix
+++ flake.nix
@@ -18,7 +18,7 @@
     };
   };
 
-  outputs = { self, nixpkgs, home-manager, nix-on-droid }: {
+  outputs = { self, nixpkgs, nixpkgs-us, home-manager, nix-on-droid }: {
 
     nixOnDroidConfigurations.default = nix-on-droid.lib.nixOnDroidConfiguration {
       pkgs = import nixpkgs { system = "aarch64-linux"; };
